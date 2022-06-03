512d163185522526df408b63c3fc8e5b4135cb26
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2022-06-02 22:22:44 -0300

    Validations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  has_many :topics
+  has_many :apprenticeships
+  validates :first_name, :last_name, :email, :gender, :birth_date, presence: true
+  validates :first_name, length: { minimum: 2 }
+  validates :gender, inclusion: { in: %w(male female), message: "%{value} is not a gender" }
+  validates :email, uniqueness: true
+
+  enum gender: { male: 0, female: 1 }
 
   def full_name
     "#{self.first_name} #{self.last_name}"
